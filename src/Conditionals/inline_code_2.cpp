9a38ea7fd7aaf503fdbc2b22dbab8d34030439c0
Craig Robbins <crobbins@localhost.localdomain>
2013-08-03 12:54:25 +1000

    Minor changes a) Replaced usages of '\n' with endl to be consistent with rest of code b) Added double line break for case 3 so output is similiar to cases 1 and 2

--- src/Conditionals/inline_code_2.cpp
+++ src/Conditionals/inline_code_2.cpp
@@ -7,34 +7,35 @@ int main()
     int choice;
     double a = 5.1, b = 4.2;
 
-    cout << "<1> Addition\n";
-    cout << "<2> Subtraction\n";
-    cout << "<3> Compare\n";
+    cout << "<1> Addition" << endl;
+    cout << "<2> Subtraction" << endl;
+    cout << "<3> Compare" << endl;
 
     cout << "Type the number of your desired option:\t";
     cin >> choice;
 
-    switch(choice)
-    {
-        case 1:
-    cout << "The result of this addition is " << a + b << endl << endl;
-    break;
+    switch (choice) {
+    case 1:
+        cout << "The result of this addition is " << a + b << endl << endl;
+        break;
 
-        case 2:
-    cout << "The result of this subtraction is " << a - b << endl << endl;
-    break;
+    case 2:
+        cout << "The result of this subtraction is " << a - b << endl << endl;
+        break;
 
-        case 3:
-            if (a > b)
-        cout << "A is greater than B";
-            else if (a < b)
-        cout << "A is less than B";
-    else //a == b
-        cout << "A equals B";
-    break;
+    case 3:
+        if (a > b) {
+            cout << "A is greater than B";
+        } else if (a < b) {
+            cout << "A is less than B";
+        } else { //a == b
+            cout << "A equals B";
+        }
+        cout << endl << endl;
+        break;
 
-        default:
-            cout << "Not an option";
+    default:
+        cout << "Not an option";
         break;
     }
 
