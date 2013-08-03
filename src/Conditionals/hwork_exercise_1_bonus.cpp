e795ab6bf75e65b5cab211a7cdab09607c4969f6
Craig Robbins <crobbins@localhost.localdomain>
2013-08-03 12:57:01 +1000

    Original homework src code from .odt file

---
+++ src/Conditionals/hwork_exercise_1_bonus.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input1,input2;
+    cout << "enter a number: ";
+    cin >> input1;
+    cout << "enter a number to compare to the first: ";
+    cin >> input2;
+    if(input1>input2)
+        cout << input1 << " is greater than " << input2;
+    else if(input1==input2)
+        cout << input1 << " is equal to " << input2;
+    else
+        cout << input2 << " is greater than " << input1;
+}
