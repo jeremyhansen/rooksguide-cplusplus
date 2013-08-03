9f41c28a9018fb098802395cc166aadb8735bb51
Craig Robbins <crobbins@localhost.localdomain>
2013-08-03 12:40:16 +1000

    Chapter Comments HWork Exercise Original with astyle applied

---
+++ src/Comments/hwork_exercise_1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    double shots, goals, saves, save_perc;
+    char cont;
+
+    do {
+
+        cout.unsetf(ios::fixed);
+        cout.unsetf(ios::showpoint);
+
+        cout << "Enter the number of shots on goal:\t";
+        cin >> shots;
+        cout << "Enter the number of goals scored:\t";
+        cin >> goals;
+        cout << endl;
+
+        saves = shots - goals;
+
+        save_perc = (saves / shots);
+
+        cout << "If there were " << shots << " shots and " << goals << " goals\n";
+        cout << "then the goalie's save percentage was ";
+
+        cout.setf(ios::fixed);
+        cout.setf(ios::showpoint);
+        cout.precision(3);
+
+        cout << save_perc << endl << endl;
+
+        cout << "Run again? Y/N\t";
+        cin >> cont;
+        cont = toupper(cont);
+        cout << endl;
+
+    } while (cont == 'Y');
+
+    system("PAUSE");
+    return 0;
+}
