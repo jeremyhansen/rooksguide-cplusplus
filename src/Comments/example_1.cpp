4e365941c334b71e49effc0d3260a9fcde0cb1b2
Craig Robbins <crobbins@localhost.localdomain>
2013-08-03 12:43:16 +1000

    Original with astyle applied

---
+++ src/Comments/example_1.cpp
@@ -0,0 +1,21 @@
+//Commenting at the beginning of the file
+//Allows you to give a summary of your program
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;  //this cout statement outputs to the screen
+    cout << “What’s the date ? ” << endl;
+
+    //comments should be used to explain things that may not be
+    //obvious to someone other than programmer
+
+    cin >> date; //takes the date from the user
+    /* you can also use comments to remind yourself of changes you want to make
+    *ie. output date to screen
+    *or debug code passed this point */
+
+    return 0;
+}
